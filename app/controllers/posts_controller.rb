44a593360b2e5b77bf3e2ad5f89e36d63aa75086
Harry Chen <harrychenca@gmail.com>
2015-05-05 15:04:07 +0800

    feat(CRUD): Complete full CRUD for Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,48 @@ class PostsController < ApplicationController
     @post.save
   end
 
+  def update
+    @post = Post.find_by_id(params[:id])
+
+    if @post.nil?
+      render :json => {
+        :message => { :message => "Cannot find post", :updated => false }
+      }
+    else
+      @post.update(post_params)
+    end
+  end
+
+  def show
+    @post = Post.find_by_id(params[:id])
+    
+    if @post.nil?
+      render :json => {
+        :message => { :message => "Cannot find post" }
+      }
+    end
+  end
+
+  def destroy
+    @post = Post.find_by_id(params[:id])
+
+    if @post.nil?
+      render :json => {
+        :message => { :message => "Cannot find post", :deleted => false }
+      }
+    else
+      if @post.destroy
+        render :json => {
+          :message => { :message => "Successful", :deleted => true }
+        }
+      else
+        render :json => {
+          :message => { :message => "Unsuccessful", :deleted => false }
+        }
+      end
+    end
+  end
+
   private
 
   def post_params
--- app/views/posts/index.jbuilder
+++ app/views/posts/index.jbuilder
@@ -1,4 +1,5 @@
 json.array! @posts do |post|
+  json.id post.id
   json.title post.title
   json.content post.content
   json.category post.category
