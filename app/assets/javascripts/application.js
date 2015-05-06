ce954717a58f3ce88269c145143c20fded27ab14
Harry Chen <harrychenca@gmail.com>
2015-05-06 23:21:26 +0800

    feat(devise): Ajax request to Devise in AngularJS works

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require underscore/underscore
-//= require angular/angular
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require underscore/underscore
+//= require angular/angular
+//= require angular-route/angular-route
 //= require_tree .
