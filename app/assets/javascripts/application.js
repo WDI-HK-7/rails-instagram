9dab34b88ff215e2f21a4e36783f1daf981f5307
Harry Chen <harrychenca@gmail.com>
2015-05-06 22:20:29 +0800

    feat(Bower): Add Bower and AngularJS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require underscore/underscore
+//= require angular/angular
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
